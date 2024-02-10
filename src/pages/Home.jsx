ae7ffdd2a554d0d8c443b6a9f36f62f93dd013c3
Shikhar Mall <shmall.21.2020@gmail.com>
2024-02-11 01:36:23 +0530

    final bug solved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -92,7 +92,7 @@ export default function Home() {
 
           <button
             type="submit"
-            className="text-white bg-teal-500 hover:bg-teal-600 font-medium rounded-lg text-sm px-5 text-center"
+            className="text-white bg-teal-500 hover:bg-teal-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             onClick={searchHandler}
           >
             Search
